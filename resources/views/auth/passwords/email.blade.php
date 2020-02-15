85b95c766c0e6457c4822355c6a958dff77f89ca
headcruser <headcruser@gmail.com>
2020-02-15 13:36:47 -0600

    Cambio Diseño
    - Cambio logotipos (favicon)
    - Cambio Idioma
    - Cambio layouts auth

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,13 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.form')
+
+@section('title', __('Reset Password') )
+@section('subtitle', 'Ingresa tus datos para recuperar tu contraseña.')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
+<div class="container mt--9 pb-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-7">
+            <div class="card bg-secondary shadow border-0">
+                <div class="card-body px-lg-12 py-lg-12">
+
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -20,7 +23,7 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -32,13 +35,14 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary btn-block">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
                         </div>
                     </form>
+
                 </div>
             </div>
         </div>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,13 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.form')
+
+@section('title', __('Reset Password') )
+@section('subtitle', 'Ingresa tus datos para recuperar tu contraseña.')
 
 @section('content')
-<div class="container">
+<div class="container mt--9 pb-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
+                <div class="card-body ">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
