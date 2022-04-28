7cac32153811b873e4463b15adb4df4d84d1927f
headcruser <headcruser@gmail.com>
2022-04-28 10:20:38 -0500

    Firma electronica: Ajuste de rutas
    - Se cambia la ruta utilizada

--- config/firma-electronica.php
+++ config/firma-electronica.php
@@ -2,11 +2,11 @@
 
 return [
     'fonts' => [
-        'nombre'    => public_path('vendor/firma-electronica/fonts/Constantia.ttf'),
-        'puesto'    => public_path('vendor/firma-electronica/fonts/Gabriola.ttf'),
-        'contacto'  => public_path('vendor/firma-electronica/fonts/Arial.ttf'),
+        'nombre'    => realpath(public_path('vendor/firma-electronica/fonts/Constantia.ttf')),
+        'puesto'    => realpath(public_path('vendor/firma-electronica/fonts/Gabriola.ttf')),
+        'contacto'  => realpath(public_path('vendor/firma-electronica/fonts/Arial.ttf')),
     ],
-    'template'  => public_path('vendor/firma-electronica/img/template.png'),
+    'template'  => realpath(public_path('vendor/firma-electronica/img/template.png')),
     'sitio-web' => 'www.covemex.com',
     'extension' => 'Tel: 413-690-1021  Ext. {extension}',
     'correo'    => 'Mail: {correo}',
