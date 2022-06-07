cd1710c08159bfd823fd5f14f748b852ea8bfb16
headcruser <headcruser@gmail.com>
2022-06-07 14:28:37 -0500

    Se agrega datatables a ext telefonicas
    - Se agrega librerias para generar pdf
    - Se agrega datatables para busqueda

--- resources/views/gestion-inventarios/reportes/extensiones_telefonicas/index.blade.php
+++ resources/views/gestion-inventarios/reportes/extensiones_telefonicas/index.blade.php
@@ -13,6 +13,12 @@
     </ol>
 @endsection
 
+@section('styles')
+    @parent
+    <link rel="stylesheet" href="{{ asset('vendor/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+@endsection
+
 @section('content')
     <div class="container-fluid">
         <div class="card">
@@ -20,7 +26,7 @@
                 <h3 class="card-title">Extensiones Registradas en <mark>Personal</mark></h3>
             </div>
             <div class="card-body">
-                <div class="row">
+                <div class="row pb-2">
                     <div class="col align-self-start">
                         <label>
                             {!! Form::select('sucursal',$sucursales, request('sucursal'), ['class' => 'custom-select custom-select-sm','form' => 'form-filter-sucursales','placeholder' => 'Todas las sucursales']) !!}
@@ -37,7 +43,7 @@
 
                 </div>
 
-                <table class="table table-bordered table-striped">
+                <table class="table table-bordered table-striped" id="tb-extensiones" style="width: 100%">
                     <thead>
                         <tr>
                             <th>Nombre</th>
@@ -63,7 +69,61 @@
 @endsection
 
 @push('scripts')
-    <script type="text/javascript">
+    <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('vendor/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('vendor/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
 
+
+    <script type="text/javascript">
+        $('#tb-extensiones').DataTable({
+            paging: false,
+            info: false,
+            dom: "<'row'<'col-xs-12 col-sm-6 d-flex align-items-center justify-content-center justify-content-sm-start'B><'col-xs-12 col-sm-6'f>><'row'<'col-12 table-responsive p-0'tr>><'row'<'col-xs-12 col-sm-7'i><'col-xs-12 col-sm-5 align-self-end d-flex justify-content-center justify-content-sm-end'p>>",
+            buttons: [
+                {
+                    title:'PDF_Extensiones',
+                    extend: 'pdfHtml5',
+                    className:'btn-sm' ,
+                    text: 'PDF',
+                     customize : function(doc){
+                        var colCount = new Array();
+
+                        $('#tb-extensiones').find('tbody tr:first-child td').each(function(){
+                            if ($(this).attr('colspan')) {
+                                for(var i=1;i<=$(this).attr('colspan');i++) {
+                                    colCount.push('*');
+                                }
+                            }else{ colCount.push('*'); }
+                        });
+
+                        // 👉 [*,*,*,*] ESPECIFICA QUE LA COLUMNA TENGA 100%
+                        doc.content[1].table.widths = colCount;
+                    }
+                },
+            ],
+            order: [[ 0, "asc" ]],
+            language: {
+                "lengthMenu": "Mostrar _MENU_ registros por pagina",
+                "zeroRecords": "No se encontro ningún registro",
+                "info": "Mostrando del _START_ al _END_ de _TOTAL_ registros. (Página _PAGE_ de _PAGES_)",
+                "infoEmpty": "No hay registros disponibles",
+                "infoFiltered": "(Filtrado de un total de _MAX_ registros)",
+                "search": "Buscar:",
+                "paginate": {
+                    "first": "Primera",
+                    "last": "Última",
+                    "next": "<i class='fa fa-chevron-right'></i>",
+                    "previous": "<i class='fa fa-chevron-left'></i>"
+                },
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+            },
+        } );
     </script>
 @endpush
